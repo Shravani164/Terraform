89c617da9a3672ee4e9e822a17985e602477ae05
shravani164 <shravanimallela12@gmail.com>
2023-09-27 15:40:44 +0530

    ec2 ecomm server

---
+++ aws_ec2.tf
@@ -0,0 +1,12 @@
+#aws ec2 instance 
+resource "aws_instance" "ecomm-web" {
+  ami           = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.ecomm-pub-sn.id
+  vpc_security_group_ids = [aws_security_group.ecomm-pub-sg.id]
+  key_name = "aws-key"
+  user_data = file("ecomm.sh")
+  tags = {
+    Name = "tf-example"
+  }
+}
